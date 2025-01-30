97e2920bc8f9b98dcc8e91adc63db2cfa2b1c949
DEV VIKNESH A D <164396475+Devviknesh@users.noreply.github.com>
2025-01-30 18:46:19 +0530

    Create voice_assistant.py

---
+++ voice_assistant.py
@@ -0,0 +1,11 @@
+import speech_recognition as sr
+
+recognizer = sr.Recognizer()
+with sr.Microphone() as source:
+    print("Listening...")
+    audio = recognizer.listen(source)
+    try:
+        text = recognizer.recognize_google(audio)
+        print("You said:", text)
+    except:
+        print("Sorry, I couldn't understand.")
